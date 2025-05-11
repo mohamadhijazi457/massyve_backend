f4030a5b1bbce82e340220e70a3464c7d9e002a9
mohamadhijazi457 <mohamadhijazi457@gmail.con>
2025-05-11 14:38:46 -0700

    modifying tsconfig

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,14 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './services/db';
 const app = express();
-app.use(cors({
-  origin: process.env.NODE_ENV === 'production' ? 'https://your-frontend-app.herokuapp.com' : 'http://localhost:3000',
-  credentials: true
-}));
-app.use(express.urlencoded({ extended: true }))
+// app.use(cors({
+//   origin: process.env.NODE_ENV === 'production' ? 'https://your-frontend-app.herokuapp.com' : 'http://localhost:3000',
+//   credentials: true
+// }));
+// app.use(express.urlencoded({ extended: true }))
+
+app.use(cors({ origin: true, credentials: true }));
+
 
 
 import authRoutes from './auth/authRoute';
